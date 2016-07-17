399af7c68741b64bb87a10fa22bb7377ea55b9c6
Trevor Merrifield <trevorm42@gmail.com>
2016-07-17 18:34:52 -0400

    Add heap.hpp

---
+++ heap/heap.hpp
@@ -0,0 +1,44 @@
+#include <functional>
+#include <vector>
+
+#ifndef HEAP_HPP
+#define HEAP_HPP
+
+template <class T>
+class Heap {
+public:
+    // Construct an empty heap.
+    Heap(std::function<bool(T, T)> comp);
+
+    // Construct a heap from a vector and consume it in the process, aka heapify.
+    // time: n
+    Heap(std::vector<T> array, std::function<bool(T, T)> comp);
+
+    // Move a node down until it is ordered with respect to its children.
+    // time: lg n
+    void bubble_down(int i);
+
+    // Move a node up until it is ordered with respect to its parent.
+    // time: lg n
+    void bubble_up(int i);
+
+    // Insert an element, preserving the heap property.
+    // time: lg n
+    void insert(T elem);
+
+    // Pop off the top element, preserving the heap property.
+    // time: constant
+    void pop();
+
+    // Heap sort the internal array.
+    // time: n * lg n
+    void heapSort();
+
+    // Get a reference to the internal array.
+    std::vector<T> & arrayRef();
+private:
+    std::vector<T> array;
+    std::function<bool(T, T)> comp;
+};
+
+#endif
